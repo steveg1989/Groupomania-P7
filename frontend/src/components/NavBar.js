f7908008a4d4d4ac16fd5791859aa75a92aef027
steveg1989 <steveg1989@gmail.com>
2022-05-22 17:19:02 -0400

    Navbar with logo

---
+++ frontend/src/components/NavBar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NavBar = () => {
+  return (
+		<div className="nav-bar">
+			<img className="logo" src="./assets/icon-left-font-monochrome-black.png" alt="logo" />
+		</div>
+	);
+};
+
+export default NavBar;
