7f787d8bc7efadd6d0cae723148da64972e2de4a
steveg1989 <steveg1989@gmail.com>
2022-05-23 18:26:01 -0400

    updated changed

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { useContext } from "react";
+import { UserContext } from "../components/AppContext";
+import Log from "../components/Log";
 
 const Home = () => {
-	return <div>HOME</div>;
+	const userConnection = useContext(UserContext);
+
+	return <div>{userConnection ? <h1>POSTS</h1> : <Log />}</div>;
 };
 
 export default Home;
