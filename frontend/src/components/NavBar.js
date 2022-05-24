302fd226e6a0a343950134e5621866525c584043
steveg1989 <steveg1989@gmail.com>
2022-05-23 20:42:14 -0400

    fixed class name to nav-container

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,8 +1,9 @@
 import React from "react";
 
+
 const NavBar = () => {
   return (
-		<div className="nav-bar">
+		<div className="nav-container">
 			<img className="logo" src="./assets/icon-left-font-monochrome-black.png" alt="logo" />
 		</div>
 	);
