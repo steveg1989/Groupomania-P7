c0679877a3b48183a2fc96a7a408ef5dddf9cdaa
steveg1989 <steveg1989@gmail.com>
2022-06-05 18:28:53 -0400

    adjustments

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,10 +1,50 @@
 import React from "react";
-
+import { NavLink } from "react-router-dom";
+import Logout from "./Log/Logout";
+import { useContext } from "react";
+import { UserContext } from "./AppContext";
+// import axios from "axios";
 
 const NavBar = () => {
+    const userId = useContext(UserContext);
+
+// const getUserName = async () => {
+	// 	await axios({
+	// 		method: "get",
+	// 		url: `${process.env.REACT_APP_API_URL}api/user/1`,
+	// 		withCredentials: true,
+	// 	})
+	// 		.then((res) => {
+	// 			console.log(res);
+	// 		})
+	// 		.catch((err) => {
+	// 			console.log(err);
+	// 		});
+	// };
+	// getUserName();
+
+
+
   return (
 		<div className="nav-container">
-			<img className="logo" src="./assets/icon-left-font-monochrome-black.png" alt="logo" />
+      <ul></ul>
+      <NavLink to="/">
+			<img className="logo" src="./assets/logos/icon-left-font-monochrome-black.png" alt="logo" />
+      </NavLink>
+      {userId ? (
+				<ul>
+					<li></li>
+					<li className="welcome">
+						<NavLink to="/profile">
+							<h5>Welcome USER</h5>
+              <img className="profile-pic" src="./assets/img/default-profile.jpeg" alt="profile-pic" />
+						</NavLink>
+					</li>
+					<Logout />
+				</ul>
+			) : (
+				<ul></ul>
+			)}
 		</div>
 	);
 };
