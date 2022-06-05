b7f3972e772eef22fd1b99497c4f06d1a422e111
steveg1989 <steveg1989@gmail.com>
2022-06-05 18:04:34 -0400

    adjustments

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -2,10 +2,11 @@ import React, { useContext } from "react";
 import { UserContext } from "../components/AppContext";
 import Log from "../components/Log";
 
-const Home = () => {
-	const userConnection = useContext(UserContext);
 
-	return <div>{userConnection ? <h1>POSTS</h1> : <Log />}</div>;
+const Home = () => {
+	const userId = useContext(UserContext);
+
+	return <div>{!userId ? <Log /> : <h1>POSTS</h1>}</div>;
 };
 
 export default Home;
